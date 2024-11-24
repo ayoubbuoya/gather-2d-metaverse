32849eba94efd44224c2997392d7efa8647fbf8b
Ayoub Amer <ayouub.ameur@gmail.com>
2024-11-24 10:01:02 +0100

    feat: full http pkg

---
+++ apps/http/src/middleware/admin.ts
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+import { NextFunction, Request, Response } from "express";
+import { JWT_PASSWORD } from "../lib/config";
+
+export const adminMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const header = req.headers["authorization"];
+  const token = header?.split(" ")[1];
+
+  if (!token) {
+    res.status(403).json({ message: "Unauthorized" });
+    return;
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_PASSWORD) as {
+      role: string;
+      userId: string;
+    };
+    if (decoded.role !== "Admin") {
+      res.status(403).json({ message: "Unauthorized" });
+      return;
+    }
+    req.userId = decoded.userId;
+    next();
+  } catch (e) {
+    res.status(401).json({ message: "Unauthorized" });
+    return;
+  }
+};
